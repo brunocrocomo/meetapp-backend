ae29854c0f8d0cbc0664d7d07cd78fae3e89c446
Bruno Martins Crocomo <bruno.crocomo@gmail.com>
2019-08-30 22:44:04 -0300

    Add filtered meetup list route

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -2,8 +2,31 @@ import * as Yup from 'yup';
 import { Op } from 'sequelize';
 import { isBefore, startOfDay, endOfDay, parseISO } from 'date-fns';
 import Meetup from '../models/Meetup';
+import User from '../models/User';
 
 class MeetupController {
+    async index(req, res) {
+        const where = {};
+        const page = req.query.page || 1;
+
+        if (req.query.date) {
+            const searchDate = parseISO(req.query.date);
+
+            where.date = {
+                [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+            };
+        }
+
+        const meetups = await Meetup.findAll({
+            where,
+            include: [User],
+            limit: 10,
+            offset: 10 * page - 10,
+        });
+
+        return res.json(meetups);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             title: Yup.string().required(),
