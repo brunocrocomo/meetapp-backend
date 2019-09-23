c3124c748f4083893d386a736bf0d87272d917f9
Bruno Martins Crocomo <bruno.crocomo@gmail.com>
2019-09-23 09:29:04 -0300

    Add file association on meetup model

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,9 +1,13 @@
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class OrganizingController {
     async index(req, res) {
         const meetups = await Meetup.findAll({
             where: { user_id: req.userId },
+            include: [
+                { model: File, as: 'file', attributes: ['id', 'path', 'url'] },
+            ],
             order: ['date'],
         });
 
